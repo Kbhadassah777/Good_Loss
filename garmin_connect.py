84693361611924c1795771fc4b2af8bd1d27c616
Kbhadassah777 <98080207+Kbhadassah777@users.noreply.github.com>
2024-02-26 22:04:34 -0700

    Plot update

--- garmin_connect.py
+++ garmin_connect.py
@@ -11,16 +11,24 @@ class garmin_connect_class:
      pass
   def get_steps(self,email,password):
     try:
-        garth.resume(GARTH_HOME)
-        garth.client.username
+          garth.resume(GARTH_HOME)
+          garth.client.username
     except (FileNotFoundError, GarthException):
-        email = input("Email: ")
-        password = getpass("Password: ")
-        garth.client.login(email,password)
+
+      email = input("Email: ")
+      password = getpass("Password: ")
+      garth.client.login(email,password)
 
     garth.save(GARTH_HOME)
+    Daily_steps = garth.DailySteps.list(period=28)
+    df = pd.DataFrame(Daily_steps).sort_values("calendar_date")
 
+    
+    
+    year = 365 * 3
+    steps_year=pd.DataFrame(garth.DailySteps.list(period=year))
+    steps_year.set_index("calendar_date", inplace=True)
+    steps_year.rename(columns={"value": "steps"}, inplace=True)
     garmin_step=pd.DataFrame(garth.DailySteps.list(date.today() - timedelta(days=1)))
     steps_bmr=(garmin_step['total_steps'])
-    return (steps_bmr[0])
-
+    return ([steps_bmr[0],df])
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
 import tkinter
-from tkinter import messagebox
+from tkinter import *
+import pandas as pd
+import seaborn as sns
+import matplotlib.dates as mdates
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+from datetime import date, timedelta
+from datetime import timedelta
 from garmin_connect import garmin_connect_class
 from cal_burnt import cal_burnt_class
 from cal_intake import cal_intake_class
@@ -16,7 +27,7 @@ if __name__ == '__main__':
     root.state('zoomed')
 
     root.geometry('340x440')
-    root.configure(bg='#ccd5ae')
+    root.configure(bg='#9d8189')
 
     def get_value():
         email_=email.get()
@@ -27,8 +38,10 @@ if __name__ == '__main__':
         snack_=snack.get()
         bmr = burnt.calculate_bmr()
         step=connect.get_steps(email_,password_)
-        calories_burnt = burnt.calculate_daily_calories(bmr,step)
+        calories_burnt = burnt.calculate_daily_calories(bmr,step[0])
         calories_ate=(intake.find_food(breakfast_))
+        df=step[1]
+        
         try:
             calories_ate+=intake.find_food(brunch_)
         except:
@@ -43,38 +56,43 @@ if __name__ == '__main__':
             pass
         
         print("You have "+ str(calories_burnt-calories_ate)+" calories burning for you!.")
-        label1=tkinter.Label(frame, text=f"You have {step} steps working for you!.")
+        label1=tkinter.Label(frame, text=f"You have {step[0]} steps working for you!.",bg='#9d8189')
         
-        label2=tkinter.Label(frame, text=f"You have {calories_ate} calories working against you!.")
+        label2=tkinter.Label(frame, text=f"You have eaten {calories_ate} calories!.",bg='#9d8189')
         
-        label3=tkinter.Label(frame, text=f"You have {calories_burnt-calories_ate} calories burning for you!.")
+        label3=tkinter.Label(frame, text=f"You have {calories_burnt-calories_ate} calories burning for you!.",bg='#9d8189')
 
-        label1.grid(row=6, column=0, columnspan=2, sticky="news")
-        label2.grid(row=7, column=0, columnspan=2, sticky="news")
-        label3.grid(row=8, column=0, columnspan=2, sticky="news")
-        df = pd.DataFrame({'val':[calories_burnt, calories_ate], 'columns':['out', 'in']})
-        fig1 = df.plot.bar(x='val', y='columns', rot=0)
-        canvas1 = FigureCanvasTkAgg(fig1, frame)
-        canvas1.draw()
-        canvas1.get_tk_widget().pack(side="left", fill="both", expand=True)
+        label1.grid(row=6, column=1, columnspan=2, sticky="news")
+        label2.grid(row=7, column=1, columnspan=2, sticky="news")
+        label3.grid(row=8, column=1, columnspan=2, sticky="news")
+        plt.figure(figsize=(10, 6))
+        sns.lineplot(x=df["calendar_date"], y=df["total_steps"])
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
+        plt.xticks(rotation=45)
+        plt.xlabel(None)
+        plt.ylabel(None)
+        plt.title("Daily Steps")
+        plt.show()
 
-    frame = tkinter.Frame(bg='#ccd5ae')
-
+    frame = tkinter.Frame(bg='#9d8189')
+ 
+        
 
     project_label = tkinter.Label(
-    frame, text="Good Loss", bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+    frame, text="Good Loss", bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     user_email = tkinter.Label(frame, 
-                      text = "Email:",bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+                      text = "Email:",bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     password_label = tkinter.Label(frame, 
-                      text = "Password:",bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+                      text = "Password:",bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     breakfast_label = tkinter.Label(frame, 
-                      text = "breakfast:",bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+                      text = "breakfast:",bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     brunch_label = tkinter.Label(frame, 
-                      text = "brunch:",bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+                      text = "brunch:",bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     lunch_label = tkinter.Label(frame, 
-                      text = "lunch:",bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+                      text = "lunch:",bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     snack_label = tkinter.Label(frame, 
-                      text = "snack:",bg='#ccd5ae', fg="#d4a373", font=("Arial", 30))
+                      text = "snack:",bg='#9d8189', fg="#d4a373", font=("Arial", 30))
     email = tkinter.Entry(frame,font=("Arial", 16)) 
     password = tkinter.Entry(frame, show="*",font=("Arial", 16)) 
     breakfast = tkinter.Entry(frame,font=("Arial", 16)) 
@@ -86,7 +104,7 @@ if __name__ == '__main__':
     
     b1 = tkinter.Button(frame, text = 'Final Deficit', bg="#d4a373", fg="#03071e", font=("Arial", 16),command=get_value)
 
-    project_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
+    project_label.grid(row=0, column=1, columnspan=1, sticky="news", pady=40)
     user_email.grid(row=1, column=0)
     email.grid(row=1, column=1, pady=20)
     password_label.grid(row=2, column=0)
